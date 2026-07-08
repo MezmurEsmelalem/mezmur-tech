fc07ccf672d73b30e22cf92c1ca0e168e09deadb
MezmurEsmelalem <mezhabesha@gmail.com>
2026-07-09 00:18:01 +0300

    fix cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -21,8 +21,9 @@ return [
 
     'allowed_origins' => [
     'http://localhost:5173',
-    'http://127.0.0.1:5173',],
+    'http://127.0.0.1:5173',
     'https://mezmur-tech.vercel.app',
+],
 
     'allowed_origins_patterns' => [],
 
