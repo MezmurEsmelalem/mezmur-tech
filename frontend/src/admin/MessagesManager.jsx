9fb42ab7d9b12d105db752d4fe1c957a1a4eb6b0
MezmurEsmelalem <mezhabesha@gmail.com>
2026-07-18 21:18:08 +0300

    messages manager color

--- frontend/src/admin/MessagesManager.jsx
+++ frontend/src/admin/MessagesManager.jsx
@@ -66,7 +66,7 @@ function MessagesManager() {
   return (
     <div className="max-w-6xl mx-auto p-6">
 
-      <h1 className="text-3xl font-bold mb-6">
+      <h1 className="text-3xl font-bold mb-6 text-white">
         Messages Manager
       </h1>
 
