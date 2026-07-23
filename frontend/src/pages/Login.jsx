7525f5e65f872ece9cb97bd2300206652b7bc29f
MezmurEsmelalem <mezhabesha@gmail.com>
2026-07-23 23:27:00 +0300

    change bg of login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -46,9 +46,9 @@ function Login() {
   };
 
 return (
-  <div className="flex pb-4 pt-4 justify-center bg-gray-100 dark:bg-gray-900">
+  <div className="flex pb-4 pt-4 justify-center dark:bg-gray-900">
 
-    <div className="rounded p-8 w-full max-w-sm">
+    <div className="bg-gray-300 dark:bg-gray-800 rounded p-8 w-full max-w-sm">
 
       <h1 className="text-2xl font-bold mb-6 text-center dark:text-white">
         Login
